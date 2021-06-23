1466854b47ca558cc7293fa59e499f2244794ff7
1024kb <52350477+1024-KB@users.noreply.github.com>
2021-06-23 11:32:08 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <van-notice-bar>请 fork 此示例，并重现你的问题</van-notice-bar>
+    <van-button class="my-button" type="primary">按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+.my-button {
+  margin: 15px;
+}
+</style>
